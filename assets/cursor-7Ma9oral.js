085c00405fd5419e775944f356ee53e5ee840cbc
AtlasNexusBot <laslyalexandre@gmail.com>
2026-05-21 19:11:53 +0200

    Deploy atlas-studio to /atlas-studio/ [skip ci]

--- assets/cursor-7Ma9oral.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./index-DUVGGVOV.js";import"./index.browser.esm-BPKuSdii.js";import"./vendor-dayjs-v0j1UNZp.js";import"./index-BBz1ziFo.js";import"./vendor-vue-NSDaktjZ.js";import"./vendor-axios-B8_nURbH.js";import"./vendor-vue-router-Df5ITouH.js";import"./index-9HGDSJTR.js";import"./signer-VxQoXw0x.js";import"./types-Cvj5H4DH.js";const c=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{c as cursorSvg};
